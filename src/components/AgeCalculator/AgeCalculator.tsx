19393c289f25b7ac5dd52901bc1fe5aaa29bc61d
clarkeadg <clarkeadg@yahoo.com>
2023-12-21 00:27:11 -0800

    Age logic

--- src/components/AgeCalculator/AgeCalculator.tsx
+++ src/components/AgeCalculator/AgeCalculator.tsx
@@ -97,28 +97,33 @@ const AgeCalculator = () => {
       const currentMonth = now.getMonth()+1;
       const currentDay = now.getDate();
 
+      // Calculate Years
       let years = currentYear - year;
       if (currentYear == year && currentMonth == month && currentDay < day) {
         years-=1;
       }
-
       if (currentMonth == month) {
         if (day > currentDay) {
           years-= 1;
         }
       } 
 
+      // Calculate Months
       let months = month - currentMonth;
+      console.log("months", months)
       if (months < 0) {
         months = currentMonth - month - 1;
+        if (day <= currentDay) {
+          months+= 1;
+        }      
       }
-
       if (currentMonth == month) {
         if (day > currentDay) {
           months = 11;
         }
       }      
 
+      // Calculate Days
       const monthDays = [31,isLeapYear(currentYear) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];      
       let days = currentDay - day;
       if (days < 0) {
