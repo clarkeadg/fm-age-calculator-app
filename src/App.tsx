47d1aa81eff2fb5783833f39e2df6f9a55e36b64
clarkeadg <clarkeadg@yahoo.com>
2023-12-20 16:15:51 -0800

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import AgeCalculator from './components/AgeCalculator/AgeCalculator'
 
 const App = () => {
   return (
-    <main className="relative min-h-screen pt-[155px]">
-      <AgeCalculator/>
+    <main className="relative min-h-screen pt-[115px] md:pt-[155px]">
+      <div className="px-2">
+        <AgeCalculator/>
+      </div>
       <div className="absolute bottom-0 w-full h-10 flex items-center justify-center">
         <Attribution/>
       </div>
--- src/components/AgeCalculator/AgeCalculator.tsx
+++ src/components/AgeCalculator/AgeCalculator.tsx
@@ -11,47 +11,47 @@ const AgeCalculator = () => {
   }
 
   return (
-    <div className="age-calculator w-[840px] mx-auto rounded-lg rounded-br-[200px] p-[55px]">
+    <div className="age-calculator w-[840px] max-w-full mx-auto rounded-lg rounded-br-[200px] px-[28px] py-[65px] md:p-[55px]">
       {/* Form */}
-      <div className="age-calculator-form pb-[45px] border-b-2 relative">
-        <form onSubmit={(e)=>{ e.preventDefault(); handleSubmit(); }} className={`${error ? "error" : ""} flex gap-[30px]`}>
-          <div className="">
-            <label htmlFor="day" className={`block text-[15px] uppercase tracking-[3px]`}>Day</label>
-            <input name="day" type="text" placeholder="DD" className={`text-[32px] px-[22px] py-[10px] border focus:outline-none rounded-sm w-[160px]`}/>
+      <div className="age-calculator-form pb-[82px] md:pb-[45px] border-b-2 relative">
+        <form onSubmit={(e)=>{ e.preventDefault(); handleSubmit(); }} autoComplete="off" className={`${error ? "error" : ""} flex gap-[24px] md:gap-[30px]`}>
+          <div className="leading-none">
+            <label htmlFor="day" className="block text-[15px] uppercase tracking-[3px] mb-[10px] md:mb-[15px]">Day</label>
+            <input name="day" type="text" placeholder="DD" className="text-[24px] px-[20px] py-[16px] md:text-[32px] md:px-[22px] md:py-[10px] border-2 focus:outline-none rounded-sm w-[112px] md:w-[160px]"/>
             { error && <div className="error-message italic text-[13px] py-1">
               This field is required
             </div> }
           </div>
-          <div className="">
-            <label htmlFor="month" className={`block text-[15px] uppercase tracking-[3px]`}>Month</label>
-            <input name="month" type="text" placeholder="MM" className={`text-[32px] px-[22px] py-[10px] border focus:outline-none rounded-sm w-[160px]`}/>
+          <div className="leading-none">
+            <label htmlFor="month" className="block text-[15px] uppercase tracking-[3px] mb-[10px] md:mb-[15px]">Month</label>
+            <input name="month" type="text" placeholder="MM" className="text-[24px] px-[20px] py-[16px] md:text-[32px] md:px-[22px] md:py-[10px] border-2 focus:outline-none rounded-sm w-[112px] md:w-[160px]"/>
             { error && <div className="error-message italic text-[13px] py-1">
               This field is required
             </div> }
           </div>
-          <div className="">
-            <label htmlFor="year" className={`block text-[15px] uppercase tracking-[3px]`}>Year</label>
-            <input name="year" type="text" placeholder="YYYY" className={`text-[32px] px-[22px] py-[10px] border focus:outline-none rounded-sm w-[160px]`}/>
+          <div className="leading-none">
+            <label htmlFor="year" className="block text-[15px] uppercase tracking-[3px] mb-[10px] md:mb-[15px]">Year</label>
+            <input name="year" type="text" placeholder="YYYY" className="text-[24px] px-[20px] py-[16px] md:text-[32px] md:px-[22px] md:py-[10px] border-2 focus:outline-none rounded-sm w-[112px] md:w-[160px]"/>
             { error && <div className="error-message italic text-[13px] py-1">
               This field is required
             </div> }
           </div>
-          <button type="submit" className="absolute rounded-full flex items-center justify-center p-2 -bottom-4 right-0">
+          <button type="submit" className="absolute rounded-full flex items-center justify-center w-[80px] h-[80px] -ml-[40px] -bottom-[40px] left-[50%] md:w-[96px] md:h-[96px] md:-ml-0 md:-bottom-[50px] md:left-auto md:right-[4px]">
             <img src={imageUrl} alt="down-arrow"/>
           </button>
         </form>        
       </div>
       {/* Results */}
-      <div className="age-calculator-results pt-[52px] text-[103px] font-bold italic leading-none">
-        <div className="age-calculator-results-years mb-1">
+      <div className="age-calculator-results pt-[86px] text-[72px] md:pt-[52px] md:text-[103px] font-bold italic leading-none">
+        <div className="age-calculator-results-years mb-[8px] md:mb-1 whitespace-nowrap">
           <span className="result">- -</span>
           <span className="px-[4px]">years</span>
         </div>
-        <div className="age-calculator-results-months mb-1">
+        <div className="age-calculator-results-months mb-[8px] md:mb-1 whitespace-nowrap">
           <span className="result">- -</span>
           <span className="px-[4px]">months</span>
         </div>
-        <div className="age-calculator-results-days">
+        <div className="age-calculator-results-days whitespace-nowrap">
           <span className="result">- -</span>
           <span className="px-[4px]">days</span>
         </div>
